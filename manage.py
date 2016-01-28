4699541e7e7f38c4e3cbabafa5cbb6071300600f
Jianwei Wang <jianwei.wang.chn@gmail.com>
2016-01-28 15:59:28 +0800

    Add command argument read
    run app example:./manage.py 0.0.0.0 4000
                                 host   port
    
    Signed-off-by: Jianwei Wang <jianwei.wang.chn@gmail.com>

--- manage.py
+++ manage.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
+import sys
 from app import create_app, db
 
 app = create_app()
 if __name__ == '__main__':
-    app.run(host="192.168.1.106")
+    try:
+        host = sys.argv[1]
+    except IndexError:
+        host = ''
+    try:
+        port = int(sys.argv[2])
+    except IndexError:
+        port = 5000
+    app.run(host = host, port = port)
